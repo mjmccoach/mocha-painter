d39b9c93a066ce6f113938997123d6007df40641
Matthew <m.mccoach88@gmail.com>
2025-11-21 16:20:50 +0000

    Add paintCan to decorator spec. Add test and functionality for adding a paintcan to decorator's stock.

--- decorator.js
+++ decorator.js
@@ -1,5 +1,9 @@
 const Decorator = function() {
     this.paintStock = [];
+
+    Decorator.prototype.addPaintCan = function(paintCan) {
+        this.paintStock.push(paintCan)
+    }
 }
 
 module.exports = Decorator;
--- specs/decorator_spec.js
+++ specs/decorator_spec.js
@@ -1,14 +1,22 @@
 const assert = require('assert');
 const Decorator = require('../decorator.js');
+const PaintCan = require('../paintCan.js');
 
 describe('Decorator', function() {
     let decorator;
+    let paintCan;
 
     beforeEach(function() {
         decorator = new Decorator();
+        paintCan = new PaintCan(1);
     });
 
     it('should start with an empty paint stock', function() {
         assert.strictEqual(0, decorator.paintStock.length);
+    })
+
+    it('should be able to add paint to stock', function() {
+        decorator.addPaintCan(paintCan);
+        assert.strictEqual(1, decorator.paintStock.length);
     })
 })
